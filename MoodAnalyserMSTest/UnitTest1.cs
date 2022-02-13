372b2efa5056776e5b5ecbb725fe14e01e5ed206
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-14 00:23:19 +0530

    [Arjun] Add ; Added Executed

--- MoodAnalyserMSTest/UnitTest1.cs
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -1,13 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProblem;
 
-namespace MoodAnalyserMSTest
+namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenSadMoodShouldReturnSAD()
+
         {
+
+            //Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodStatus moodAnalyse = new MoodStatus(message);
+
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+
         }
     }
 }
