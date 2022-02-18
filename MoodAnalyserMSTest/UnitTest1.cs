530c9dcfda07d91a494fc0cf97184558299245e1
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-18 08:30:31 +0530

    [Arjun] Add ; Added Executed

--- MoodAnalyserMSTest/UnitTest1.cs
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -7,6 +7,8 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
+        //Method to test sad message(UC1-TC1.1)
+        [TestCategory("SAD Message")]
         public void GivenSadMoodShouldReturnSAD()
 
         {
@@ -23,5 +25,25 @@ namespace TestProject1
             Assert.AreEqual(expected, mood);
 
         }
+        //Method to test happy message(UC1-TC1.2)
+        [TestCategory("HAPPY Message")]
+        public void GivenAnyMoodShouldReturnHAPPY()
+
+        {
+            //Arrange
+
+            string expected = "HAPPY";
+            string message = "I am in Happy Mood";
+            MoodStatus moodAnalyse = new MoodStatus(message);
+
+            //Act
+            string mood = moodAnalyse.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+
+        }
     }
 }
+    
+
